57dbbe894cc4bcd7022726c543f73f0fadcfb734
SirBeNoble <akeemperkins@gmail.com>
2024-07-25 17:33:37 -0400

    final

---
+++ src/sections/index.sections.jsx
@@ -0,0 +1,20 @@
+import Hero from "./Hero.section";
+import PopularProducts from "./PopularProducts.section";
+import SuperQuality from "./SuperQuality.section";
+import Services from "./Services.section";
+import SpecialOffers from "./SpecialOffers.section";
+import CustomerReviews from "./CustomerReviews.section";
+import Subscribe from "./Subscribe.section";
+import Footer from "./Footer.section";
+
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
